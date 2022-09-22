fb80f7836fa85be4a08bc2d455a1a79d3cc092ed
jfruit <jfruiitty@gmail.com>
2022-09-22 18:33:01 +0700

    final yk present

---
+++ js/ykpresent.js
@@ -0,0 +1,86 @@
+
+
+function kawasaki(){
+    var a = document.getElementById("motorcycle");
+    a.setAttribute("src","./images/kawasaki_custom.png");
+
+    var b = document.getElementById("motor-logo");
+    b.setAttribute("src","./images/kawasaki-logo.png");
+
+}
+
+function suzuki(){
+    var a = document.getElementById("motorcycle");
+    a.setAttribute("src","./images/suzuki_custom.png");
+
+    var b = document.getElementById("motor-logo");
+    b.setAttribute("src","./images/suzuki-logo.png");
+
+}
+
+function honda(){
+    var a = document.getElementById("motorcycle");
+    a.setAttribute("src","./images/honda_custom.png");
+
+    var b = document.getElementById("motor-logo");
+    b.setAttribute("src","./images/honda-logo.png");
+
+}
+
+function yamaha(){
+    var a = document.getElementById("motorcycle");
+    a.setAttribute("src","./images/yamaha_custom.png");
+
+    var b = document.getElementById("motor-logo");
+    b.setAttribute("src","./images/yamaha-logo.png");
+
+}
+
+function rimsilver(){
+    var a = document.getElementById("rim");
+    a.setAttribute("src","./images/rim-silver-excel.png");
+
+    var b = document.getElementById("rim-logo");
+    b.setAttribute("src","./images/excel_silver.png");
+}
+
+function rimblack(){
+    var a = document.getElementById("rim");
+    a.setAttribute("src","./images/rim-black-excel.png");
+
+    var b = document.getElementById("rim-logo");
+    b.setAttribute("src","./images/excel_black.png");
+}
+
+function rimblue(){
+    var a = document.getElementById("rim");
+    a.setAttribute("src","./images/rim-blue-excel.png");
+
+    var b = document.getElementById("rim-logo");
+    b.setAttribute("src","./images/excel-blue.png");
+}
+
+function hubsilver(){
+    var a = document.getElementById("hub");
+    a.setAttribute("src","./images/silver-hub.png");
+
+    var b = document.getElementById("hub-logo");
+    b.setAttribute("src","./images/silver.png");
+}
+
+function hubblack(){
+    var a = document.getElementById("hub");
+    a.setAttribute("src","./images/black-hub.png");
+
+    var b = document.getElementById("hub-logo");
+    b.setAttribute("src","./images/black.png");
+}
+
+function hubblue(){
+    var a = document.getElementById("hub");
+    a.setAttribute("src","./images/blue-hub.png");
+
+    var b = document.getElementById("hub-logo");
+    b.setAttribute("src","./images/blue.png");
+}
+
